d9f20fb652f07dc9295c7cfdb7df2de0593b002c
bbrk24 <bbrk24@gmail.com>
2023-10-10 19:33:24 -0400

    Add baro sensor

--- sketch_oct9a.ino
+++ sketch_oct9a.ino
@@ -1,12 +1,42 @@
+
+#include <Adafruit_MPL3115A2.h>
+
+static Adafruit_MPL3115A2 baro;
+static bool begun = false;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+
+  begun = baro.begin();
+  if (begun) {
+    // idk, copied from the example
+    baro.setSeaPressure(1013.26);
+  } else {
+    Serial.println("Failed to set up baro sensor.");
+  }
+
+  // Pin A1: analog input from VOC sensor
   pinMode(1, PinMode::INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   delay(500);
   int v = analogRead(1);
-  Serial.println(v);
+
+  Serial.print("VOC voltage: ");
+  Serial.println(3.3F / 1023.0F * (float)v);
+
+  if (begun) {
+    float altitude = baro.getAltitude();
+    Serial.print("Altitude (meters): ");
+    Serial.println(altitude);
+
+    float pressure = baro.getPressure();
+    Serial.print("Pressure (hPa): ");
+    Serial.println(pressure);
+    
+    float temperature = baro.getTemperature();
+    Serial.print("Temperature: ");
+    Serial.println(temperature);
+  }
 }
