a1dbf3760c526ad8bfb1f838caa0dc6231f9a4e9
DarthSethious1 <sethmoe11@gmail.com>
2023-11-16 11:22:53 -0500

    Comments
    Just adding some relatively simple comments to help understand code for gps.h and payload_bay.h

--- payload_bay_micro.ino
+++ payload_bay_micro.ino
@@ -8,8 +8,8 @@ Altimeter alt;
 
 void setup() {
   pinMode(8, OUTPUT);
-  digitalWrite(8, LOW);
-  do {
+  digitalWrite(8, LOW); //Sets up pin 8 to be the signal to eject capsules
+  do { //Until the altimeter is active, attempt to initialize
     alt.initialize();
   } while (alt.getStatus() != Altimeter::ACTIVE);
 }
@@ -26,16 +26,16 @@ void loop() {
 
   switch (mode) {
   case BELOW_5K:
-    delay(1000);
+    delay(1000); //Wait 1 second
     if (alt.getAltitude() >= 30.0F) {
       mode = WATCHING;
     }
     break;
   case WATCHING:
     data.addPoint(alt.getAltitude());
-    if (data.isDecreasing()) {
-      digitalWrite(8, HIGH);
-      mode = PAST_APOGEE;
+    if (data.isDecreasing()) { //If we notice our altitude is decreasing, we've reached apogee
+      digitalWrite(8, HIGH); //Flip ejection pin high
+      mode = PAST_APOGEE; //Put us in 'PAST_APOGEE' mode
     }
     break;
   case PAST_APOGEE:
