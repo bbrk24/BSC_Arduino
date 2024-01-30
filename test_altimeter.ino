6140324cb42595d5e8d89045f9c81940557d3c21
TheIrishBearcat24 <clarkeck@mail.uc.edu>
2024-01-30 11:44:39 -0500

    altimeter testing buffer readings

--- test_altimeter.ino
+++ test_altimeter.ino
@@ -1,8 +1,12 @@
-#if false
+#if true
+
+#define BUFFER_SIZE 15
 
 #include "altimeter.h"
+#include "Buffer.h"
 
 Altimeter alt;
+Buffer buf;
 
 void setup() {
   Serial.begin(9600);
@@ -25,15 +29,20 @@ void loop() {
     alt.initialize();
   } else {
     const float SEA_LEVEL_PRESSURE = 1013.25F; // sea level pressure in hPa
-    alt.setSeaLevel(SEA_LEVEL_PRESSURE);
+    // alt.setSeaLevel(SEA_LEVEL_PRESSURE);
     float altitude = alt.getAltitude();
+    buf.addPoint(altitude);
+    Serial.print("Recent max: ");
+    Serial.print(buf.maximum());
+    Serial.print(", min: ");
+    Serial.println(buf.minimum());
 
     // checks if we're above our origin launch altitude
-    if (altitude > 0) {
+    if (altitude > -2) {
       Serial.print("Altitude: ");
       Serial.print(altitude);
-      Serial.println("m");
-      delay(102);
+      Serial.println(" ft");
+      delay(1000);
     } else {
       Serial.print("Rocket has not gone up into the air. Trying again in ");
       Serial.print(backoff);
