d778fd4d98fe7b3ec8e9e4c9fa9dc5cc7465adb8
TheRoyalIrishman <TheRoyalIrishman@users.noreply.github.com>
2024-02-01 12:17:16 -0500

    VOC sensor code working

--- test_voc.ino
+++ test_voc.ino
@@ -1,14 +1,17 @@
-#if false
+#if true
 
 void setup() {
 	Serial.begin(9600);
+  // this uses the AREF pin to use 3.3 V instead of 5.0 V
+  analogReference(EXTERNAL);
 	while (!Serial) { /* do nothing here */ }
 }
 
 void loop() {
   delay(500);
 
-  int v = analogRead(6);
+  // had to change to pin 4 because of using Arduino Micro
+  int v = analogRead(4);
   Serial.print("VOC voltage (V): ");
   Serial.println(3.3F / 1023.0F * (float)v);
 }
