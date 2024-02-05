eea7e67db3166151ac0a4e62229a117c8a2d5d0d
TheRoyalIrishman <TheRoyalIrishman@users.noreply.github.com>
2024-02-05 13:09:16 -0500

    Plotting progress

--- csv_stats.py
+++ csv_stats.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 from sklearn.linear_model import LinearRegression
+from math import sqrt
 
 # read in rocketry CSV file output by Arduino
 
@@ -18,37 +19,171 @@ else:
     print("Capsule two columns not found, handling alignment...")
 
 # converts latitude output to actual coordinates
-normalizedLatitude = csv_file_dataframe["Latitude"] / pow(10, 7)
+
+normalized_latitude = csv_file_dataframe["Latitude"] / pow(10, 7)
 
 # converts longitude output to actual coordinates
-normalizedLongitude = csv_file_dataframe["Longitude"] / pow(10, 7)
 
-# picks the column data to plot with x's
+normalized_longitude = csv_file_dataframe["Longitude"] / pow(10, 7)
+
+# grabs MSL Altitude
+
+msl_altitude = csv_file_dataframe["Altitude (MSL)"]
+
+# grabs num satellites
+
+num_satelites = csv_file_dataframe["Satellites"]
+
+# grabs the X Acceleration component from CSV file
+
+accel_x = csv_file_dataframe["Accel X"]
+
+# grabs the Y Acceleration component from CSV file
+
+accel_y = csv_file_dataframe["Accel Y"]
+
+# grabs the Z Acceleration component from CSV file
+
+accel_z = csv_file_dataframe["Accel Z"]
+
+# grabs X acceleration for gyro
+
+gyro_x = csv_file_dataframe["Gyro X"]
+
+# grabs Y acceleration for gyro
+
+gyro_y = csv_file_dataframe["Gyro Y"]
+
+# grabs Z acceleration for gyro
+
+gyro_z = csv_file_dataframe["Gyro Z"]
+
+# IMU data plotting
+
+accel_mag = []
+for i in range(len(accel_x)):
+    x = accel_x.iloc[i]
+    y = accel_y.iloc[i]
+    z = accel_z.iloc[i]
+    accel_mag.append(sqrt(x*x + y*y + z*z))
+
+accel_np_array = np.array(accel_mag)
+
+# sets row index to be expressed in 50 millisecond increments
+row_index = np.arange(len(csv_file_dataframe)).reshape(-1, 1) / 50
+
+plt.figure()
+plt.plot(row_index, accel_np_array)
+plt.title("IMU Acceleration Magnitude (m/s^2)")
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.grid(True)
+plt.show()
+
+# altitude plotting
+
 altitude_column = "Altitude (AGL)"
-csv_file_dataframe[altitude_column].plot(marker = "x")
-
-# Create X and Y arrays for linear regression
-X = np.arange(len(csv_file_dataframe)).reshape(-1, 1)
-Y = csv_file_dataframe[altitude_column].values.reshape(-1, 1)
+row_index = np.arange(len(csv_file_dataframe)).reshape(-1, 1)
+altitude_output = csv_file_dataframe[altitude_column].values.reshape(-1, 1)
 
 # Create a linear regression model
-model = LinearRegression()
-model.fit(X, Y)
+altimeter_model_regression = LinearRegression()
+altimeter_model_regression.fit(row_index, altitude_output)
 
 # Predict Y values using the model
-Y_pred = model.predict(X)
+altimeter_pred = altimeter_model_regression.predict(row_index)
 
-# Calculate R^2 value and slope
-r2 = r2_score(Y, Y_pred)
+# # Calculate R^2 value and slope
+r2 = r2_score(altitude_output, altimeter_pred)
 # I have no earthly idea why the slope is defined like this, but this is what the internet said
-slope_output = model.coef_[0][0]
+slope_output = altimeter_model_regression.coef_[0][0]
 
 # Plot the linear regression line
-plt.plot(X, Y_pred, color='red', label=f'Linear Regression\nR^2 = {r2:.5f}\nSlope (ft/min) = {slope_output * 60:.5f}')
+plt.figure() # this creates a new figure to differentiate between altimeter and IMU graphs
+plt.plot(row_index, altimeter_pred, color='red', label=f'Linear Regression\nR^2 = {r2:.5f}\nSlope (ft/min) = {slope_output * 60:.5f}')
 
 # Display other plot details
 plt.xlabel("Row Index")
 plt.ylabel(f"{altitude_column} (ft)")
 plt.title(f"Plot of {altitude_column} vs Row Index")
 plt.legend()
+plt.show()
+
+# Gyro plotting
+
+gyro_mag = []
+
+# gyro_x, gyro_y, and gyro_z all have the same length, so it doesn't matter which variable you use
+for i in range(len(gyro_x)):
+    x = gyro_x.iloc[i]
+    y = gyro_y.iloc[i]
+    z = gyro_z.iloc[i]
+    gyro_mag.append(sqrt(x*x + y*y + z*z))
+
+gyro_np_array = np.array(gyro_mag)
+
+plt.figure()
+plt.plot(row_index, gyro_np_array)
+plt.title("Plot of Gyro Acceleration")
+plt.xlabel("Time (s)")
+plt.ylabel("Acceleration (m/s^2)")
+plt.grid(True)
+plt.show()
+
+# mean sea level altitude plotting
+
+altitude_msl_column = "Altitude (MSL)"
+altitude_msl_output = csv_file_dataframe[altitude_msl_column].values.reshape(-1, 1)
+
+# Create a linear regression model
+altimeter_msl_model_regression = LinearRegression()
+altimeter_msl_model_regression.fit(row_index, altitude_msl_output)
+
+# Predict Y values using the model
+altimeter_msl_pred = altimeter_msl_model_regression.predict(row_index)
+
+# # Calculate R^2 value and slope
+r2_msl = r2_score(altitude_msl_output, altimeter_msl_pred)
+# I have no earthly idea why the slope is defined like this, but this is what the internet said
+slope_msl_output = altimeter_msl_model_regression.coef_[0][0]
+
+# Plot the linear regression line
+plt.figure() # this creates a new figure to differentiate between altimeter and IMU graphs
+plt.plot(row_index, altimeter_msl_pred, color='red', label=f'Linear Regression\nR^2 = {r2:.5f}\nSlope (ft/min) = {slope_msl_output * 60:.5f}')
+
+# Display other plot details
+plt.xlabel("Row Index")
+plt.ylabel(f"{altitude_msl_column} (ft)")
+plt.title(f"Plot of {altitude_msl_column} vs Row Index")
+plt.legend()
+plt.show()
+
+# latitude plotting
+
+plt.figure()
+plt.plot(row_index, normalized_latitude)
+plt.title("Plot of Row Index vs Latitude")
+plt.xlabel("Row Index")
+plt.ylabel("Latitude")
+plt.grid(True)
+plt.show()
+
+# Longitude plotting
+
+plt.figure()
+plt.plot(row_index, normalized_longitude)
+plt.title("Plot of Row Index vs Longitude")
+plt.xlabel("Row Index")
+plt.ylabel("Longitude")
+plt.grid(True)
+plt.show()
+
+# Satellite plotting
+
+plt.figure()
+plt.plot(row_index, num_satelites)
+plt.title("Plot of Row Index vs Satellites")
+plt.xlabel("Row Index")
+plt.ylabel("Satellites")
+plt.grid(True)
 plt.show()
