0d8c9ab31a8abf51812371b1869aa72d93da8b93
TheRoyalIrishman <TheRoyalIrishman@users.noreply.github.com>
2024-02-28 11:18:32 -0500

    A new constant to check against count threshold

--- payload_relay.ino
+++ payload_relay.ino
@@ -1,10 +1,18 @@
 #if false
 
+#ifndef PAYLOAD_THRESHOLD
 #define PAYLOAD_THRESHOLD 14.5 // height of one floor in Baldwin staircase in units of feet
+#endif
+
+#ifndef COUNT_THRESHOLD
+#define COUNT_THRESHOLD 200
+#endif
 
 #include "Altimeter.h"
 
 Altimeter m_altimeter;
+
+int numAltimeterReadings = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,9 +27,11 @@ void loop() {
     delay(1000);
     m_altimeter.initialize();
   } else {
-    float altitude_output = m_altimeter.getAltitude();
+    float altitudeOutput = m_altimeter.getAltitude();
 
-    if (altitude_output > PAYLOAD_THRESHOLD) {
+    numAltimeterReadings += 1;
+
+    if (altitudeOutput > PAYLOAD_THRESHOLD && numAltimeterReadings > COUNT_THRESHOLD) {
       // if threshold is exceeded. then turn on relay
       pinMode(8, OUTPUT);
       digitalWrite(8, LOW); //Sets up pin 8 to be the signal to eject capsules
