4ca64698a3e0f4d1670da32372f484233e9398f0
TheRoyalIrishman <TheRoyalIrishman@users.noreply.github.com>
2024-02-28 19:47:12 -0500

    Seth changes

--- payload_relay.ino
+++ payload_relay.ino
@@ -1,14 +1,9 @@
 #if false
 
-#ifndef PAYLOAD_THRESHOLD
-#define PAYLOAD_THRESHOLD 14.5 // height of one floor in Baldwin staircase in units of feet
-#endif
-
-#ifndef COUNT_THRESHOLD
-#define COUNT_THRESHOLD 200
-#endif
-
 #include "Altimeter.h"
+
+const float PAYLOAD_THRESHOLD = 14.5; // the height of one floor in Baldwin Hall staircase (ft)
+const float COUNT_THRESHOLD 200; // I THINK this is how we rate limit the frequency?
 
 Altimeter m_altimeter;
 
@@ -19,6 +14,7 @@ void setup() {
   while (!Serial) { /* Wait for Serial monitor to connect */ }
   Serial.println("Connecting to sensor...");
   m_altimeter.initialize();
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
@@ -29,12 +25,10 @@ void loop() {
   } else {
     float altitudeOutput = m_altimeter.getAltitude();
 
-    numAltimeterReadings += 1;
-
     if (altitudeOutput > PAYLOAD_THRESHOLD && numAltimeterReadings > COUNT_THRESHOLD) {
-      // if threshold is exceeded. then turn on relay
-      pinMode(8, OUTPUT);
       digitalWrite(8, LOW); //Sets up pin 8 to be the signal to eject capsules
     }
+
+    numAltimeterReadings += 1;
   }
 }
